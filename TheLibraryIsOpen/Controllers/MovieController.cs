4c1a0b521ab1c53c5c71b8e15b9a76fc4a012a97
SandraRoz <Bella.roz22@gmail.com>
2018-10-02 22:43:17 -0400

    WIP added more movieController methods

--- TheLibraryIsOpen/Controllers/MovieController.cs
+++ TheLibraryIsOpen/Controllers/MovieController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TheLibraryIsOpen.Models;
 using TheLibraryIsOpen.Models.DBModels;
 
 namespace TheLibraryIsOpen.Controllers
@@ -11,9 +13,41 @@ namespace TheLibraryIsOpen.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["Message"] = "Your movie index page.";
             return View();
         }
 
+        public IActionResult Create()
+        {
+            ViewData["Message"] = "Your create movie page.";
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+
+            ViewData["Message"] = "Your delete movie page.";
+            return View();
+        }
+
+
+        public IActionResult Edit()
+        {
+            ViewData["Message"] = "Your edit movie page.";
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            ViewData["Message"] = "Your movie list page.";
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
 
     }
 }
