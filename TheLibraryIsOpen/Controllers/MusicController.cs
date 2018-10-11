d534c42a48c8623a81ffa19cde56495c489f2066
MBenchimol <32246260+MBenchimol@users.noreply.github.com>
2018-10-11 11:07:56 -0400

    Initial commit for the classes needed for music controller and catalog

---
+++ TheLibraryIsOpen/Controllers/MusicController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheLibraryIsOpen.Controllers
+{
+    public class MusicController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AddNewMusic()
+        {
+            ViewData["Message"] = "Add Music";
+
+            return View();
+        }
+    
+        public IActionResult DeleteMusic()
+        {
+            ViewData["Message"] = "Delete Music";
+
+            return View();
+        }
+        public IActionResult EditMusic()
+        {
+            ViewData["Message"] = "Edit Music";
+
+            return View();
+        }
+    }
+}
---
+++ TheLibraryIsOpen/Controllers/StorageManagement/MusicCatalog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TheLibraryIsOpen.Database;
+using TheLibraryIsOpen.Models.DBModels;
+
+namespace TheLibraryIsOpen.Controllers.StorageManagement
+{
+    public class MusicCatalog : ControllerBase
+    {
+        private readonly Db _db;
+
+        public MusicCatalog(Db db)
+        {
+            _db = db;
+        }
+
+        public bool AddNewMusic(string type, string title, string artist, string label, string releaseDate, string asin)
+        {
+            Music m = new Music();
+
+            bool result = Db.createMusic(m);
+
+            return result;
+        }
+    }
+}
