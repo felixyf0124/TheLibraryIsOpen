64ee09fbe7ed4638f07eb218dd8c6c82072fa9b6
Albert <albertnaggear@gmail.com>
2018-11-02 00:11:52 -0400

    view improvements + added search bar

--- TheLibraryIsOpen/Controllers/HomeController.cs
+++ TheLibraryIsOpen/Controllers/HomeController.cs
@@ -1,20 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using TheLibraryIsOpen.Models;
-using TheLibraryIsOpen.Models.DBModels;
 using TheLibraryIsOpen.Controllers.StorageManagement;
 using TheLibraryIsOpen.Database;
+using TheLibraryIsOpen.Models;
+using TheLibraryIsOpen.Models.DBModels;
 
 
 namespace TheLibraryIsOpen.Controllers
 {
-	public class HomeController : Controller
-	{
+    public class HomeController : Controller
+    {
         private readonly ClientStore _cs;
         private readonly BookCatalog _bookc;
         private readonly MusicCatalog _musicc;
@@ -29,35 +29,35 @@ namespace TheLibraryIsOpen.Controllers
             _musicc = muc;
             _magazinec = mac;
         }
-	    
+
         public IActionResult Index()
-		{
-			return View();
-		}
+        {
+            return View();
+        }
 
         public IActionResult Book()
         {
             return View();
         }
 
-		public IActionResult About()
-		{
-			ViewData["Message"] = "Your application description page.";
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
 
-			return View();
-		}
+            return View();
+        }
 
-		public IActionResult Contact()
-		{
-			ViewData["Message"] = "Your contact page.";
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
 
-			return View();
-		} 
+            return View();
+        }
 
-		public IActionResult Privacy()
-		{
-			return View();
-		}
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
         public async Task<ActionResult> Catalog()
         {
@@ -65,39 +65,7 @@ namespace TheLibraryIsOpen.Controllers
             return View(list);
         }
 
-        // For test db.cs
-        public void ListTest()
-	    {
-	        Db _db = new Db();
-            /*List<Book> allBooksList = _db.GetAllBooks();
-	        foreach (var book in allBooksList as List<TheLibraryIsOpen.Models.DBModels.Book>)
-	        {
-	            Console.Write(book.ToString());
-	        }*/
-/*
-
-	        List<Magazine> allMgsList = _db.GetAllMagazines();
-	        foreach (var mg in allMgsList as List<TheLibraryIsOpen.Models.DBModels.Magazine>)
-	        {
-	            Console.Write(mg.ToString());
-	        }*/
-	        
-/*
-
-            Magazine mg1 = new Magazine("test23","test3","test2","test2","test2","test2");
-            _db.UpdateMagazine(mg1,1);
-            _db.DeleteMagazineByID(3);*/
-
-           /*Book book2 = new Book("Do Android Dream of Electric Sheep?","Philip K. Dick","Paperback",55555,"Del Rey","Sept .26 2017","English","1524796972","978-1524796976");
-            //_db.CreateBook(book2);
-            _db.UpdateBookByBookIsbn(book2, "1524796972");*/
-            //_db.DeleteBookByIsbn10("1524796972");
-	        Magazine book1 = _db.GetMagazineByIsbn13("2323");
-	        Console.Write(book1.ToString());
-
-        }
-
-	    public async Task<ActionResult> ListOfClients()
+        public async Task<ActionResult> ListOfClients()
         {
             string clientEmail = User.Identity.Name;
             bool isAdmin = await _cs.IsItAdminAsync(clientEmail);
@@ -115,9 +83,24 @@ namespace TheLibraryIsOpen.Controllers
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Search()
+        {
+            var form = HttpContext.Request.Form;
+            string modeltype = form["modeltype"];
+            string query = form["query"];
+
+            TempData["ModelType"] = modeltype;
+            TempData["Query"] = query;
+
+            //method not complete
+
+            return View();
+        }
+    }
 }
--- TheLibraryIsOpen/wwwroot/js/site.js
+++ TheLibraryIsOpen/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+    $('.dropdown').hover(function () {
+        $(this).addClass('open');
+    },
+        function () {
+            $(this).removeClass('open');
+        });
+});
