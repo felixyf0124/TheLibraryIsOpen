8c98bae22f1a2764a700372e34bbd0e6b79146f9
Albert <albertnaggear@gmail.com>
2018-11-03 09:08:12 -0400

    Corrections: SearchResult in its own file.

---
+++ TheLibraryIsOpen/Models/SearchResult.cs
@@ -0,0 +1,26 @@
+﻿using static TheLibraryIsOpen.Constants.TypeConstants;
+
+namespace TheLibraryIsOpen.Models
+{
+    public class SearchResult
+    {
+        public TypeEnum Type { get; set; }
+        public int ModelId { get; set; }
+        public string Name { get; set; }
+        public string[] Description { get; set; }
+
+        //Default constructor
+        public SearchResult() { }
+
+        //constructor
+        public SearchResult(TypeEnum _type, int _modelId, string _name, string[] _description)
+        {
+            Type = _type;
+            ModelId = _modelId;
+            Name = _name;
+            Description = _description;
+        }
+
+    }
+
+}
