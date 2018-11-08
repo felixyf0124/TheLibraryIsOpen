aa341b780a4b3e836d4530b747d3249d9bce68b9
Antoine B <antoine.beiten@gmail.com>
2018-11-07 23:45:27 -0500

    #96 added various ModelCopy methods in DB
    * FindModelCopiesOfClient
    * Find BookByModelCopy
    * Find MagazineByModelCopy
    * Find MovieByModelCopy
    * Find MusicByModelCopy
    *FindModelCopiesOfModel
    * created skeleton of CountModelCopiesOfModel
    
    Also added Enum borrowed types in Constants

--- TheLibraryIsOpen/Constants/Constants.cs
+++ TheLibraryIsOpen/Constants/Constants.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using TheLibraryIsOpen.Models.DBModels;
 
 namespace TheLibraryIsOpen.Constants
@@ -15,6 +13,14 @@ namespace TheLibraryIsOpen.Constants
             Movie = 2,
             Music = 3,
             Person = 4
+        };
+
+        public enum BorrowType
+        {
+            Borrowed = 0,
+            NotBorrowed = 1,
+            Any = 3
+
         };
 
         private static Dictionary<Type, TypeEnum> typeDict = new Dictionary<Type, TypeEnum>
