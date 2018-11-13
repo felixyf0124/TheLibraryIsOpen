83f0b43ff90c8dcaf1053428e9bb5d7105f42045
Albert <albertnaggear@gmail.com>
2018-11-13 00:52:46 -0500

    added session logic to save complex objects, + sessionmodel for models

---
+++ TheLibraryIsOpen/Constants/SessionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace TheLibraryIsOpen.Constants
+{
+    public static class SessionExtensions
+    {
+        public static void SetObject(this ISession session, string key, object obj)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(obj));
+        }
+
+        public static T GetObject<T>(this ISession session, string key)
+        {
+            string jsonObject = session.GetString(key);
+            return jsonObject is null ? default(T) : JsonConvert.DeserializeObject<T>(jsonObject);
+        }
+    }
+}
---
+++ TheLibraryIsOpen/Models/SessionModel.cs
@@ -0,0 +1,10 @@
+﻿using static TheLibraryIsOpen.Constants.TypeConstants;
+
+namespace TheLibraryIsOpen.Models
+{
+    public class SessionModel
+    {
+        public int Id { get; set; }
+        public TypeEnum ModelType { get; set; }
+    }
+}
