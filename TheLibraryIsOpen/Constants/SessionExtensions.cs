b6f858c7eae01ec248fcc4585f03d9af1342b8c7
Albert <albertnaggear@gmail.com>
2018-11-13 01:20:18 -0500

    added xaml comments

--- TheLibraryIsOpen/Constants/SessionExtensions.cs
+++ TheLibraryIsOpen/Constants/SessionExtensions.cs
@@ -3,13 +3,39 @@ using Newtonsoft.Json;
 
 namespace TheLibraryIsOpen.Constants
 {
+    /// <summary>
+    /// Allows for complex object storage
+    /// </summary>
     public static class SessionExtensions
     {
+        /// <summary>
+        /// Stores the object to the session
+        /// </summary>
+        /// <param name="key">The name of the object in the session</param>
+        /// <param name="obj">The object to store to the session</param>
+        /// <seealso cref="TheLibraryIsOpen.Models.SessionModel" />
+        /// <code>
+        /// var books = HttpContext.Session.GetObject&lt;List&lt;SessionModel&gt;&gt;("Books") ?? new List&lt;SessionModel&gt;();
+        /// </code>
         public static void SetObject(this ISession session, string key, object obj)
         {
             session.SetString(key, JsonConvert.SerializeObject(obj));
         }
 
+        /// <summary>
+        /// Retrieves the object from the session
+        /// </summary>
+        /// <param name="key">The name of the object in the session</param>
+        /// <seealso cref="TheLibraryIsOpen.Models.SessionModel" />
+        /// <code>
+        /// SessionModel sm = new SessionModel {
+        ///     Id = book.Id,
+        ///     ModelType = TypeEnum.Book
+        /// }
+        /// var books = HttpContext.Session.GetObject&lt;List&lt;SessionModel&gt;&gt;("Books") ?? new List&lt;SessionModel&gt;();
+        /// books.Add(sm);
+        /// HttpContext.Session.SetObject("Books", books);
+        /// </code>
         public static T GetObject<T>(this ISession session, string key)
         {
             string jsonObject = session.GetString(key);
--- TheLibraryIsOpen/Models/SessionModel.cs
+++ TheLibraryIsOpen/Models/SessionModel.cs
@@ -2,6 +2,10 @@
 
 namespace TheLibraryIsOpen.Models
 {
+    /// <summary>
+    /// Stores necessary model information to keep in session
+    /// </summary>
+    /// <seealso cref="TheLibraryIsOpen.Constants.SessionExtensions"/>
     public class SessionModel
     {
         public int Id { get; set; }
