80f5f5db79133d01ac37220f59de14465949ba27
MBenchimol <32246260+MBenchimol@users.noreply.github.com>
2018-11-16 23:34:52 -0500

    Implement Controller and view for transactions

---
+++ TheLibraryIsOpen/Controllers/StorageManagement/TransactionCatalog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheLibraryIsOpen.Database;
+using TheLibraryIsOpen.Models.DBModels;
+
+namespace TheLibraryIsOpen.Controllers.StorageManagement
+{
+    public class TransactionCatalog
+    {
+        private readonly Db _db;
+
+        public TransactionCatalog(Db db)
+        {
+            _db = db;
+        }
+
+        public Task<List<Log>> getLogs()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                return _db.GetAllLogs();
+            });
+        }
+    }
+}
---
+++ TheLibraryIsOpen/Controllers/TransactionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TheLibraryIsOpen.Controllers.StorageManagement;
+
+namespace TheLibraryIsOpen.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly TransactionCatalog _tc;
+
+        public TransactionController(TransactionCatalog tc)
+        {
+            _tc = tc;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _tc.getLogs());
+        }
+    }
+}
