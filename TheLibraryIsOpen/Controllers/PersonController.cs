87de42737016adf508445619791561143d63c840
vincent.sun870530@gmail.com <vincent.sun870530@gmail.com>
2018-11-19 11:27:13 -0500

    add person view and controller

---
+++ TheLibraryIsOpen/Controllers/PersonController.cs
@@ -0,0 +1,190 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TheLibraryIsOpen.Controllers.StorageManagement;
+using TheLibraryIsOpen.Models;
+using TheLibraryIsOpen.Models.DBModels;
+using static TheLibraryIsOpen.Constants.SessionExtensions;
+using static TheLibraryIsOpen.Constants.TypeConstants;
+
+namespace TheLibraryIsOpen.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly PersonCatalog _mc;
+        private readonly ClientStore _cs;
+
+        public PersonController(PersonCatalog mc, ClientStore cs)
+        {
+            _mc = mc;
+            _cs = cs;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            bool isAdmin = await _cs.IsItAdminAsync(User.Identity.Name);
+            if (!isAdmin)
+                return Unauthorized();
+            List<Person> li = await _mc.GetAllPersonDataAsync();
+            return View(li);
+        }
+
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = await _mc.FindByIdAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            //TempData["AvailableCopies"] = await _mc.getNoOfAvailableModelCopies(magazine);
+
+            return View(person);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isAdmin = await _cs.IsItAdminAsync(User.Identity.Name);
+                if (!isAdmin)
+                    return Unauthorized();
+                await _mc.CreateAsync(person);
+                await _mc.CommitAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            bool isAdmin = await _cs.IsItAdminAsync(User.Identity.Name);
+            if (!isAdmin)
+                return Unauthorized();
+
+            var person = await _mc.FindByIdAsync(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+            return View(person);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, Person person)
+        {
+            if (int.Parse(id) != person.PersonId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    bool isAdmin = await _cs.IsItAdminAsync(User.Identity.Name);
+                    if (!isAdmin)
+                        return Unauthorized();
+                    await _mc.UpdateAsync(person);
+                    await _mc.CommitAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MagazineExists(id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(person);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            bool isAdmin = await _cs.IsItAdminAsync(User.Identity.Name);
+            if (!isAdmin)
+                return Unauthorized();
+            var person = await _mc.FindByIdAsync(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(Person person)
+        {
+            bool isAdmin = await _cs.IsItAdminAsync(User.Identity.Name);
+            if (!isAdmin)
+                return Unauthorized();
+            await _mc.DeleteAsync(person);
+            await _mc.CommitAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult AddToCart(int id)
+        {
+            var Items = HttpContext.Session.GetObject<List<SessionModel>>("Items")
+                ?? new List<SessionModel>();
+            Items.Add(new SessionModel { Id = id, ModelType = TypeEnum.Magazine});
+            HttpContext.Session.SetObject("Items", Items);
+            HttpContext.Session.SetInt32("ItemsCount", Items.Count);
+            return RedirectToAction(nameof(Details), new { id = id.ToString() });
+        }
+
+        private bool MagazineExists(string id)
+        {
+            return (_mc.FindByIdAsync(id) != null);
+        }
+
+        public async Task<IActionResult> AddModelCopy(string id)
+        {
+            await _mc.addModelCopy(id);
+            await _mc.CommitAsync();
+            return RedirectToAction(nameof(Details), new { id = id.ToString() });
+        }
+
+        public async Task<IActionResult> DeleteModelCopy(string id)
+        {
+            await _mc.deleteFreeModelCopy(id);
+            await _mc.CommitAsync();
+            return RedirectToAction(nameof(Details), new { id = id.ToString() });
+        }
+    }
+}
---
+++ TheLibraryIsOpen/Controllers/StorageManagement/PersonCatalog.cs
@@ -0,0 +1,184 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TheLibraryIsOpen.Database; // TODO: delete this when db code is removed
+using TheLibraryIsOpen.db;
+using TheLibraryIsOpen.Models.DBModels;
+using static TheLibraryIsOpen.Constants.TypeConstants;
+
+namespace TheLibraryIsOpen.Controllers.StorageManagement
+{
+    public class PersonCatalog
+    {
+        private readonly UnitOfWork _unitOfWork;
+        private readonly IdentityMap _im;
+        private readonly Db _db; // TODO: delete this when db code is removed
+
+
+        public PersonCatalog(UnitOfWork unitOfWork, IdentityMap im, Db db)
+        {
+            _unitOfWork = unitOfWork;
+            _im = im;
+            _db = db; // TODO: delete this when db code is removed
+        }
+
+        //Create Person
+        public Task<IdentityResult> CreateAsync(Person person)
+        {
+            if (person != null)
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    // TODO: manage error if register return false
+                    _unitOfWork.RegisterNew(person);
+                    return IdentityResult.Success;
+                });
+            }
+            return Task.Factory.StartNew(() =>
+            {
+                return IdentityResult.Failed(new IdentityError { Description = "person was null" });
+            });
+        }
+
+
+        //Delete Magazine
+        public Task<IdentityResult> DeleteAsync(Person person)
+        {
+            if (person != null)
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    _unitOfWork.RegisterDeleted(person);
+                    return IdentityResult.Success;
+                });
+            }
+            return Task.Factory.StartNew(() =>
+            {
+                return IdentityResult.Failed(new IdentityError { Description = "person was null" });
+            });
+        }
+
+        public void Dispose()
+        { }
+
+
+        //Find methods (by id, isbn10, isbn13)
+
+        public async Task<Person> FindByIdAsync(string personID)
+        {
+            Person person = await _im.FindPerson(int.Parse(personID));
+
+            return person;
+        }
+
+        public Task<Magazine> FindByIsbn10Async(string isbn10)
+        {
+            if (!string.IsNullOrEmpty(isbn10))
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    // TODO: replace with _im
+                    return _db.GetMagazineByIsbn10(isbn10);
+                });
+            }
+            throw new ArgumentNullException("isbn10");
+        }
+
+        public Task<Magazine> FindByIsbn13Async(string isbn13)
+        {
+            if (!string.IsNullOrEmpty(isbn13))
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    // TODO: replace with _im
+                    return _db.GetMagazineByIsbn13(isbn13);
+                });
+            }
+            throw new ArgumentNullException("isbn13");
+        }
+
+
+        public Task<IdentityResult> UpdateAsync(Person person)
+        {
+            if (person != null)
+            {
+                return Task.Factory.StartNew(() =>
+                {
+                    _unitOfWork.RegisterDirty(person);
+                    return IdentityResult.Success;
+                });
+            }
+            return Task.Factory.StartNew(() =>
+            {
+                return IdentityResult.Failed(new IdentityError { Description = "person was null" });
+            });
+        }
+
+
+        //Get all person
+        public Task<List<Person>> GetAllPersonDataAsync()
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                // TODO: replace with _im
+                return _db.GetAllPerson();
+            });
+        }
+
+        public Task<bool> CommitAsync()
+        {
+            return _unitOfWork.CommitAsync();
+        }
+
+        public Task<int> getNoOfAvailableModelCopies(Magazine magazine)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+
+                int AvailableCopies = _db.CountModelCopiesOfModel(magazine.MagazineId, (int)TypeEnum.Magazine, BorrowType.NotBorrowed);
+
+                return AvailableCopies;
+
+            });
+
+        }
+
+        public async Task<List<ModelCopy>> getModelCopies(Magazine magazine)
+        {
+            List<ModelCopy> copies = await _im.FindModelCopies(magazine.MagazineId, TypeEnum.Magazine);
+            
+            return copies;
+
+        }
+
+        public Task<IdentityResult> addModelCopy(string id)
+        {
+                return Task.Factory.StartNew(() =>
+                {
+                    // TODO: manage error if register returns false
+
+                    _unitOfWork.RegisterNew(new ModelCopy
+                    {
+                        modelID = Int32.Parse(id),
+                        modelType = TypeEnum.Magazine
+                    });
+                    return IdentityResult.Success;
+                });
+        }
+        public Task<IdentityResult> deleteFreeModelCopy(string id)
+        {
+                return Task.Factory.StartNew(() =>
+                {
+                    // TODO: manage error if register returns false
+                    ModelCopy temp = new ModelCopy
+                    {
+                        modelID = Int32.Parse(id),
+                        modelType = TypeEnum.Magazine
+                    };
+                    _im.DeleteFreeModelCopy(temp, Int32.Parse(id));
+                    return IdentityResult.Success;
+                });
+        }
+    }
+}
