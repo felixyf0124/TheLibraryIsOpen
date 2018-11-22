b7c9020d6ede56fe7c08453b4e20a148fe2816d0
Jessica Allaire <jessica.allaire.96@hotmail.com>
2018-11-21 20:00:46 -0500

    Just pushing changes, basic files

---
+++ TheLibraryIsOpen/Controllers/ReturnController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TheLibraryIsOpen.Constants;
+using TheLibraryIsOpen.Controllers.StorageManagement;
+using TheLibraryIsOpen.db;
+using TheLibraryIsOpen.Models;
+using TheLibraryIsOpen.Models.Return;
+using TheLibraryIsOpen.Models.DBModels;
+using static TheLibraryIsOpen.Constants.TypeConstants;
+
+namespace TheLibraryIsOpen.Controllers
+{
+    public class ReturnController : Controller
+    {
+        private readonly ClientManager _cm;
+        private readonly BookCatalog _bookc;
+        private readonly MusicCatalog _musicc;
+        private readonly MovieCatalog _moviec;
+        private readonly MagazineCatalog _magazinec;
+        private readonly IdentityMap _identityMap;
+
+        public ReturnController(ClientManager cm, BookCatalog bc, MusicCatalog muc, MovieCatalog moc, MagazineCatalog mac, IdentityMap imap)
+        {
+            _cm = cm;
+            _bookc = bc;
+            _moviec = moc;
+            _musicc = muc;
+            _magazinec = mac;
+            _identityMap = imap;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TheLibraryIsOpen/Models/Return/ReturnViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static TheLibraryIsOpen.Constants.TypeConstants;
+
+namespace TheLibraryIsOpen.Models.Return
+{
+    public class ReturnViewModel
+    {
+        public int ModelId { get; set; }
+        public string Title { get; set; }
+        public TypeEnum Type { get; set; }
+
+    }
+}
